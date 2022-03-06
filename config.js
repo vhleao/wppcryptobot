8c1c991316078eb9c6c6033aea86c0351712c67f
vhleao <100950033+vhleao@users.noreply.github.com>
2022-03-06 12:43:58 -0300

    Add files

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = config = {
+    key: "3e5ba4f6-2a5c-4d2d-9916-0f977c7b6496",
+}
---
+++ robo.js
@@ -0,0 +1,75 @@
+
+const wppconnect = require('@wppconnect-team/wppconnect');
+
+wppconnect.create({
+    session: 'whatsbot',
+    autoClose: false,
+    puppeteerOptions: { args: ['--no-sandbox'] }
+})
+    .then((client) =>
+        client.onMessage((message) => {
+            const msg = message.body;
+            console.log(message.body);
+            const cmd = '/p'
+            const config = require('./config')
+
+            if (msg.startsWith(cmd)){
+                const coin2 = msg.substring(3)
+                console.log(coin2)
+                const https = require('https');
+                var coin = 'bcoin'
+                const options = 
+                    {hostname: 'pro-api.coinmarketcap.com',
+                    path: `/v2/cryptocurrency/quotes/latest?symbol=${coin2}` ,
+                    headers: {"X-CMC_PRO_API_KEY":config.key}
+                    }
+
+                https.get(options, (resp) => {
+                    var code = resp.statusCode;
+                    console.log('statusCode:', code);
+                let data = '';
+
+                // A chunk of data has been received.
+                resp.on('data', (chunk) => {
+                data += chunk;
+                });
+
+                // The whole response has been received. Print out the result.
+                if(resp.statusCode == 200){
+                resp.on('end', () => {
+                    
+                    var adata = data.split('price')[1]
+                    var price = adata.substring(2, 7);
+
+                    
+                    //console.log(JSON.parse(data).data.BCOIN[0].quote.USD.price);
+                    console.log(price);
+                    sendWppMessage(client, message.from, `Preço da ${coin2}: $${price} USD`); 
+                   
+                }).on("error", (err) => {
+                    console.log("Error: " + err.message);
+                    });
+                }else{
+                    sendWppMessage(client, message.from, `REQUISIÇÃO INVÁLIDA (${code}) `); 
+                }
+                }).on("error", (err) => {
+                console.log("Error: " + err.message);
+                });
+                
+                            }
+                            
+                            
+                        }))
+                    .catch((error) =>
+                        console.log(error));
+
+
+function sendWppMessage(client, sendTo, text) {
+    client.sendText(sendTo, text)
+        .then((result) => {
+            // console.log('SUCESSO: ', result); 
+        })
+        .catch((erro) => {
+            console.error('ERRO: ', erro);
+        });
+}
